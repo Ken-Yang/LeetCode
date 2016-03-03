ef0aa972b4bd237b1ad12b346358bf46c5d86979
Ken Yang <kuas216083@gmail.com>
2016-03-03 15:15:39 +0800

    add HappyNumber

---
+++ src/net/kenyang/leetcode/HappyNumber.java
@@ -0,0 +1,41 @@
+package net.kenyang.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Write an algorithm to determine if a number is "happy". A happy number is a
+ * number defined by the following process: Starting with any positive integer,
+ * replace the number by the sum of the squares of its digits, and repeat the
+ * process until the number equals 1 (where it will stay), or it loops endlessly
+ * in a cycle which does not include 1. Those numbers for which this process
+ * ends in 1 are happy numbers. Example: 19 is a happy number 1^2 + 9^2 = 82 8^2
+ * + 2^2 = 68 6^2 + 8^2 = 100 1^2 + 0^2 + 0^2 = 1
+ * 
+ * @author kencyang
+ *
+ */
+public class HappyNumber {
+
+    public HappyNumber() {
+        System.out.println(isHappy(9));
+    }
+
+    public static void main(String[] args) {
+        new HappyNumber();
+    }
+
+    public boolean isHappy(int n) {
+        HashMap<Integer, Integer> h = new HashMap<>();
+        while (n != 1 && h.get(n) == null) {
+            h.put(n, 1);
+            String strTmp = String.valueOf(n);
+            n = 0;
+            for (char c : strTmp.toCharArray()) {
+                int iTmp = Character.getNumericValue(c);
+                n = n + (iTmp * iTmp);
+            }
+        }
+        return n == 1;
+
+    }
+}
