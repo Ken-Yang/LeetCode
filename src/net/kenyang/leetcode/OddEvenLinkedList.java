47270de8a1b2a648fafc08169117855bee0367f3
Ken Yang <ken@kenyang.net>
2016-03-09 15:05:51 +0800

    Odd Even Linked List

--- src/net/kenyang/leetcode/OddEvenLinkedList.java
+++ src/net/kenyang/leetcode/OddEvenLinkedList.java
@@ -1,5 +1,7 @@
 package net.kenyang.leetcode;
 
+import net.kenyang.leetcode.ReverseLinkedList.ListNode;
+
 /**
  * Given a singly linked list, group all odd nodes together followed by the even
  * nodes. Please note here we are talking about the node number and not the
@@ -25,6 +27,28 @@ public class OddEvenLinkedList {
     }
 
     public OddEvenLinkedList() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        ListNode node8 = new ListNode(8);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        
+        node1 = oddEvenList(node1);
+        ListNode nodetmp = node1;
+        while (nodetmp != null) {
+            System.out.println(nodetmp.val);
+            nodetmp = nodetmp.next;
+        }
     }
 
     public static void main(String[] args) {
@@ -32,8 +56,23 @@ public class OddEvenLinkedList {
     }
 
     public ListNode oddEvenList(ListNode head) {
-
-        return null;
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (odd.next != null && even.next != null) {
+            odd.next = even.next;
+            odd = even.next;
+            even.next = odd.next;
+            even = odd.next;
+        }
+        odd.next = evenHead;
+        
+        
+        return head;
     }
 
 }
